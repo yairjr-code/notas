f3bfc5bb3e4492e9acdc94dde7d9c570eecdc0bb
yairjr-code <eduardojad1102@gmail.com>
2025-02-25 06:04:59 -0500

    ES un programa de nota que verifica si una calificación es aprovada o reprovada.

---
+++ nota.py
@@ -0,0 +1,18 @@
+
+
+nota = int(input("digite la calificacion: "))
+
+if nota >= 55 and nota <= 100:
+  
+  print(f"Aprvaste: {nota}")
+elif nota >= 0 and nota <=54:
+    print("perdido")
+else: 
+   print("no pertenece a una nota valida")
+
+if nota >90:
+   print("Felicitacones")
+
+return
+
+
